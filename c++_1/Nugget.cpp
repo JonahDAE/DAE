d96440aee2061bd04022012c70c4c7b1e0c423b8
Student <Student@DAEDMAC28.localdomain>
2025-04-01 18:17:50 -0400

    Made a program in C++

---
+++ c++_1/Nugget.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    // This is a function in the code.
+    cout << "you are given a specific amount of nuggets. You look inside to see how many you have...";
+ // integer for number of nuggets
+    int nuggets = 8;
+
+ // This is the conditional statement
+    if (nuggets == 8) {
+            cout << "You have the correct amount of nuggets!!";
+        } 
+    else {
+            cout << "You don't have the correct amount of nuggets. Your public sacrifice will be in 2 weeks.";
+        }
+}
