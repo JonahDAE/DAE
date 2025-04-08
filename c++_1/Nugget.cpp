5757b07138d75792039a0a64a9c3202e213c71a4
Student <Student@DAEDMAC28.localdomain>
2025-04-08 17:32:36 -0400

    Updated C++

--- c++_1/Nugget.cpp
+++ c++_1/Nugget.cpp
@@ -2,17 +2,33 @@
 #include <string>
 using namespace std;
 
+//This is the function I'm using with a conditional function inside it
+void checking( int nuggets ) {
+  if (nuggets == 8) {
+            cout << "You have the correct amount of nuggets!!";
+        } 
+    else {
+            cout << "You don't have the correct amount of nuggets. Your public sacrifice will be in 2 weeks.";
+        }  
+}
+
 int main () {
     // This is a function in the code.
     cout << "you are given a specific amount of nuggets. You look inside to see how many you have...";
  // integer for number of nuggets
     int nuggets = 8;
+    int employee = 'Jonah';
 
- // This is the conditional statement
-    if (nuggets == 8) {
-            cout << "You have the correct amount of nuggets!!";
-        } 
-    else {
-            cout << "You don't have the correct amount of nuggets. Your public sacrifice will be in 2 weeks.";
-        }
+if (employee == 'Jonah') {
+    // What happens when the boolaean expression (question) is true
+    // Write your code below
+    cout << "Yes";
+
+
+} else {
+    // What happens when the boolaean expression (question) is false
+    // Write your code below
+        cout << "No";
+    // checking() //calling the function in main    
+
 }
