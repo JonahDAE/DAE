9eaa1c10ea7a14ff1e5ce357a0879841bd05bca9
Student <Student@DAEDMAC28.localdomain>
2025-05-15 16:36:51 -0400

    Possibly pushing to git for the final time.

---
+++ embedded_systems_1/Blink_copy_20250515163031/Blink_copy_20250515163031.ino
@@ -0,0 +1,25 @@
+int sensorValue;
+int sensoLow = 1023;
+int sensorHigh = 0;
+const int ledPin = 13;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  while (millis() < 5000) {
+   sensorValue = analogRead(AO);
+   if (sensorValue > sensorHigh) {
+     sensorHigh = sensorValue;
+   }
+   if (sensorValue < sensorLow) {
+     sensorLow = sensorValue;
+   }
+  }
+  digitalWrite(ledPin, LOW);
+}
+void loop() {
+  sensorValue = analogRead(AO):
+  int pitch =
+  map(sensorValue,sensorLow,sensorHigh, 50, 4000):
+  tone(8,pitch,20):
+  delay(10);
+}
