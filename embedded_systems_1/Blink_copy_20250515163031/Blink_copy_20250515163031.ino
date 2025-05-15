05291bb6bf653eba11b5a8483642a2eb732a6de9
Student <Student@DAEDMAC28.localdomain>
2025-05-15 17:49:46 -0400

    Last push for a while.

--- embedded_systems_1/Blink_copy_20250515163031/Blink_copy_20250515163031.ino
+++ embedded_systems_1/Blink_copy_20250515163031/Blink_copy_20250515163031.ino
@@ -1,25 +1,29 @@
 int sensorValue;
-int sensoLow = 1023;
+int sensorLow = 1023;
 int sensorHigh = 0;
 const int ledPin = 13;
+
 void setup() {
+  
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
+  
   while (millis() < 5000) {
-   sensorValue = analogRead(AO);
-   if (sensorValue > sensorHigh) {
-     sensorHigh = sensorValue;
-   }
+    sensorValue = analogRead(A0);
+    if (sensorValue > sensorHigh) {
+      sensorHigh = sensorValue;
+      }
    if (sensorValue < sensorLow) {
      sensorLow = sensorValue;
-   }
+      }
   }
   digitalWrite(ledPin, LOW);
 }
+
 void loop() {
-  sensorValue = analogRead(AO):
+  sensorValue = analogRead(A0);
   int pitch =
-  map(sensorValue,sensorLow,sensorHigh, 50, 4000):
-  tone(8,pitch,20):
+    map(sensorValue, sensorLow, sensorHigh, 50, 4000);
+  tone(8,pitch,20);
   delay(10);
 }
